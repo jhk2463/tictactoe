bf8ebd9b5475d81f52818dccb57a03bc3d4faf15
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-06-15 01:25:18 -0400

    Up to point of tutorial. Needs improvments.

---
+++ client.js
@@ -0,0 +1,166 @@
+//Create websocket object
+//Pass URL and port # of the server
+//Every time server sends a message to client, ONMESSAGE event is triggered
+//To send msg to server, SEND method of websocket object is used
+
+const sidebar = document.querySelector('.sidebar');
+const connect = document.querySelector('.connectBtn');
+const create = document.querySelector('.createBtn');
+const join = document.querySelector('.joinBtn');
+const userCol = document.querySelector('.flex-col1')
+const gamesList = document.querySelector('ul');
+const board = document.querySelector('.board');
+const cells = document.querySelectorAll('.cell');
+create.disabled = true;
+join.disabled = true;
+
+var clientId;
+var gameId;
+var socket;
+var symbol;
+
+//Event listener for clicking on 'Connect' button
+connect.addEventListener('click', (src) => {    //Click connect button to connect to server
+    socket = new WebSocket('ws://localhost:8080');
+    socket.onmessage = onMessage;
+    src.target.disabled = true;                 //Button greys out
+});
+
+//Event listener for clicking on 'Create' button
+create.addEventListener('click', () => {
+    socket.send(JSON.stringify({
+        'tag': 'create',
+        'clientId': clientId
+
+    }))
+})
+
+//Event listener for clicking on 'Join' button
+join.addEventListener('click', () => {
+    socket.send(JSON.stringify({
+        'tag': 'join',
+        'clientId': clientId,
+        'gameId': gameId
+    }))
+})
+
+//Functions to handle player making a move on the board
+function makeMove() {
+    //Add an event listener to empty cells
+    cells.forEach(cell => {
+        if(!cell.classList.contains('cross') && ! cell.classList.contains('circle')) {
+            cell.addEventListener('click', cellClicked);
+        }
+    })
+}
+function cellClicked(src) {
+    //Add player's symbol to the clicked cell
+    let icon;
+    if(symbol == 'x') {
+        icon = 'cross';
+    } else {
+        icon = 'circle';
+    }
+    src.target.classList.add(icon)
+    //Create a updated local 'board' based off 'cells' to send to server
+    const board = []
+    for(i=0; i<9; i++){
+        if(cells[i].classList.contains('circle')){
+            board[i] = 'o';
+        } else if(cells[i].classList.contains('cross')){
+            board[i] = 'x';
+        } else{
+            board[i] = '';
+        }
+    }
+    //Remove all event listeners on cells since player has made a move
+    cells.forEach(cell => {
+        cell.removeEventListener('click', cellClicked);
+    });
+    //Send updated board to server
+    socket.send(JSON.stringify({
+        'tag': 'moveMade',
+        'board': board,
+        'clientId': clientId,
+        'gameId': gameId
+    }))
+}
+
+//Function to process message received from server
+function onMessage(msg) {
+    const data = JSON.parse(msg.data);
+    //Process message from server based on 'tag'
+    switch(data.tag){
+        case 'connected':
+            clientId = data.clientId;    //Create local copy of clientId sent from server
+            userCol.innerHTML = `UserId: ${clientId}`
+            userCol.classList.add('joinLabel') //Make a label w/ client's Id number
+            create.disabled = false;
+            join.disabled = false;
+            break;
+
+        case 'gamesList':
+            const games = data.list
+            //Remove existing list and replace with new list sent from server to avoid duplicates
+            while(gamesList.firstChild){ 
+                gamesList.removeChild(gamesList.lastChild);
+            }
+            games.forEach( game => {
+                const li = document.createElement('li');
+                li.innerText = game;
+                li.style.textAlign = 'center';
+                gamesList.appendChild(li);
+                li.addEventListener('click', () => {gameId = game}) //Store last list item clicked on as client's 'gameId'
+            }) 
+            break;
+
+        case 'created':
+            gameId = data.gameId;
+            create.disabled = true;
+            join.disabled = true;
+            console.log(gameId);
+            break;
+
+        case 'joined':
+            document.querySelector('.board').style.display='grid';
+            symbol = data.symbol;
+            if(symbol == 'x'){
+                board.classList.add('cross');
+            } else {
+                board.classList.add('circle');
+            }
+            break;
+        
+        case 'updateBoard':
+            //Make clean slate for the board
+            cells.forEach(cell => {
+                if(cell.classList.contains('cross')){
+                    cell.classList.remove('cross');
+                } else if (cell.classList.contains('circle')) {
+                    cell.classList.remove('circle');
+                }
+            })
+            //Loop through board array sent by server and update on client
+            for(i=0; i<9; i++) {
+                if(data.board[i] == 'x'){
+                    cells[i].classList.add('cross');
+                } else if(data.board[i] == 'o') {
+                    cells[i].classList.add('circle');
+                }
+            }
+            //Allow player to make move if it is their turn
+            if(data.isTurn) {
+                makeMove();
+            }
+            break;
+        
+        case 'winner':
+            alert(`The winner is ${data.winner}`);
+            break;
+
+        case 'gameDraw':
+            alert('The game is a draw');
+            break;
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,160 @@
+//Create http server object & make it listen on some port
+//Pass the http server object to websocket object constructor
+//If msg received from client is 'request' to upgrade to websocket protocol
+//accept it, create unique connection with the client
+//Send and receive messages with the client using that connection object
+
+const http = require('http').createServer().listen(8080, console.log("Listening on port 8080"))
+const WebSocket = require('websocket').server;    //Import websocket 
+const wss = new WebSocket({'httpServer' : http}); //Create websocket server 
+
+var clients = {};    //Store client connections with client id as key
+var games = {};      //Store game objects with game id as key
+//Array of arrays storing all possible winning states in cell-index form
+const WIN_STATES = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+//Dealing with client request to connect to server
+wss.on('request', (req) => {
+    const conn = req.accept(null, req.origin); //Variable to store particular client connection
+    const clientId = Math.round(Math.random()*10) + Math.round(Math.random()*10) + Math.round(Math.random()*10) //Create 'unique' Id for client
+    clients[clientId] = { 'conn' : conn };     //Store clientId and connection 
+    conn.send(JSON.stringify({
+        'tag': 'connected',          //Informing client that they are connected to server
+        'clientId': clientId
+    }));
+
+    sendAvailableGames();
+
+    conn.on('message', onMessage);
+});
+
+//Function to send list of available games to each client
+function sendAvailableGames() {     
+    const availableGames = []; //Store game id of available games
+    //Find games that have only 1 player             
+    for(const game in games) {
+        if(games[game].players.length<2){
+            availableGames.push(game);
+        }
+    }
+    //Send to every client connected
+    for(const client in clients) {
+        clients[client].conn.send(JSON.stringify({
+            'tag': 'gamesList',
+            'list': availableGames
+        }));
+    }
+}
+
+//Function to send updated board to both players
+function updateBoard(gameId) {
+    games[gameId].players.forEach(player => {
+        clients[player.clientId].conn.send(JSON.stringify({
+            'tag': 'updateBoard',
+            'isTurn': player.isTurn,
+            'board': games[gameId].board
+        }))
+    })
+}
+
+//Function to check if cells of WIN_STATES are all the same symbol
+function winState(gameId) {
+    return WIN_STATES.some(state => {
+        return (state.every(cell => {
+            return games[gameId].board[cell]=='x'
+            }) ||
+            state.every(cell => {
+                return games[gameId].board[cell]=='o'
+            })
+        );
+    });
+}
+
+//Draw if every WIN_STATES has a mix of 'x' and 'o'
+function drawState(gameId) {
+    return WIN_STATES.every(state => {
+        return (
+            state.some(cell => {
+                return games[gameId].board[cell]=='x'
+            }) &&
+            state.some(cell => {
+                return games[gameId].board[cell]=='o'
+            })
+        );
+    });
+}
+
+//Functtion to process message received from client
+function onMessage(msg) {
+    const data = JSON.parse(msg.utf8Data); //Need to use 'utf8Data' since we are on server
+    switch(data.tag){
+        case 'create':
+            const gameId = Math.round(Math.random()*10) + Math.round(Math.random()*10) + Math.round(Math.random()*10) //Create 'unique' Id for game
+            const board = ['','','','','','','','',''];
+
+            const playerOne = {    //One player object
+                'clientId': data.clientId,
+                'symbol': 'x',      //Player 1 is x, player 2 is o
+                'isTurn': true
+            };
+            const players = Array(playerOne);  //For two players
+
+            games[gameId] = {   //Game object
+                'board': board,
+                'players': players
+            };
+            
+            clients[data.clientId].conn.send(JSON.stringify({  //Send client the created game
+                'tag': 'created',
+                'gameId': gameId
+            }))
+            sendAvailableGames();
+            break;
+
+        case 'join':
+            const playerTwo = {
+                'clientId': data.clientId,
+                'symbol': 'o',      //Player 1 is x, player 2 is o
+                'isTurn': false
+            }
+            games[data.gameId].players.push(playerTwo);    //Assign 2nd player to game
+            sendAvailableGames();   //Refresh list of available games
+            games[data.gameId].players.forEach(player=> {
+                clients[player.clientId].conn.send(JSON.stringify({ //Send client the joined game
+                    'tag': 'joined',
+                    'gameId': data.gameId,
+                    'symbol': player.symbol
+                }))
+            })
+            updateBoard(data.gameId);
+            break;
+
+        case 'moveMade':
+            //Update board based of msg from client
+            games[data.gameId].board = data.board;
+            //Check if someone has won, game is draw, or game is still on
+            const isWinner = winState(data.gameId);
+            const isDraw = drawState(data.gameId);
+            if(isWinner){
+                games[data.gameId].players.forEach(player => {
+                    clients[player.clientId].conn.send(JSON.stringify({
+                        'tag': 'winner',
+                        'winner': player.symbol
+                    }))
+                })
+            } else if(isDraw) {
+                games[data.gameId].players.forEach(player => {
+                    clients[player.clientId].conn.send(JSON.stringify({
+                        'tag': 'gameDraw'
+                    }))
+                })
+            } else {
+                //Flip 'isTurn' for both players
+                games[data.gameId].players.forEach(player => {
+                    player.isTurn = !player.isTurn;
+                });
+                updateBoard(data.gameId);
+            }
+            break;
+    }
+}
